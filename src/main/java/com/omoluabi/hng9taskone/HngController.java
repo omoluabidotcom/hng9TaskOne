8fecfe49315c2e0a7cb199280960274d678ef567
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-28 22:25:35 +0100

    crossorigin url change to wildcard character

--- src/main/java/com/omoluabi/hng9taskone/HngController.java
+++ src/main/java/com/omoluabi/hng9taskone/HngController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // rest controller class
-@CrossOrigin(origins = "https://myhngtaskone.herokuapp.com")
+@CrossOrigin(origins = "*")
 @RestController
 public class HngController {
 
