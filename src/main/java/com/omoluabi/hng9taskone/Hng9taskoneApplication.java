6a2a75bacf01f977cfc2dab9cbf1e1cce0f7b2c1
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-28 23:24:40 +0100

    adjust cors issue

--- src/main/java/com/omoluabi/hng9taskone/Hng9taskoneApplication.java
+++ src/main/java/com/omoluabi/hng9taskone/Hng9taskoneApplication.java
@@ -2,12 +2,31 @@ package com.omoluabi.hng9taskone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Hng9taskoneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Hng9taskoneApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("*")); // Provide list of origins if you want multiple origins
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
