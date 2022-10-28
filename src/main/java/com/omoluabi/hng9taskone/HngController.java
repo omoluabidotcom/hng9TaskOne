cc9481b05b67a9cac205515b08dca757a61ba484
omoluabidotcom <yyahaya222@yahoo.com>
2022-10-28 11:01:36 +0100

    get endpoint method complete

--- src/main/java/com/omoluabi/hng9taskone/HngController.java
+++ src/main/java/com/omoluabi/hng9taskone/HngController.java
@@ -1,4 +1,25 @@
 package com.omoluabi.hng9taskone;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// rest controller class
+@RestController
 public class HngController {
+
+    // return type autowired
+    @Autowired
+    private DetailsStructure detailsStructure;
+
+    // get endpoint
+    @GetMapping("/firsttask")
+    public DetailsStructure one(DetailsStructure detailsStructure) {
+
+        detailsStructure.setSlackUsername("OmoluabiDaGreat");
+        detailsStructure.setBackend(true);
+        detailsStructure.setAge(25);
+        detailsStructure.setBio("I am result-oriented and love to use technology to solve human problems");
+        return detailsStructure;
+    }
 }
